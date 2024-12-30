8502012ba1b1bfa97d933bf4a6b62664e762c544
Bhaskar1008 <Bhaskarkeelu.92@gmail.com>
2024-12-31 04:39:58 +0530

    Packages removed

--- packages/react/src/store/slices/reducers/setSelectedComponentReducer.ts
+++
@@ -1,9 +0,0 @@
-import { PayloadAction } from '@reduxjs/toolkit';
-import { FormState } from '../../../types';
-
-export const setSelectedComponentReducer = (
-  state: FormState,
-  action: PayloadAction<string | null>
-) => {
-  state.selectedComponent = action.payload;
-};
