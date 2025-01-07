7b34b704956cbecf5763af6cb7deccd9c35d3c17
bhaskar1008 <bhaskarkeelu.92@gmail.com>
2025-01-07 17:27:20 +0530

    vite config changes temporaryly

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,5 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
+  }
 });
